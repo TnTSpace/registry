ae376522faf3b193a667ea9c8fdd8ddf5367df28
Jumia <toolsntuts.space@gmail.com>
2025-05-17 12:34:31 +0100

    Update +page.svelte

--- src/routes/(documents)/legals/+page.svelte
+++ src/routes/(documents)/legals/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
-	let { data }: { ../legal/$typesgeData } = $props();
+	let { data }: { PageData } = $props();
 </script>
 
 <h2>Legal Documents</h2>
