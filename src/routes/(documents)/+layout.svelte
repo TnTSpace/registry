f1822bb4fb5a471df08c1c39cb07cc5b40a6ff5d
Jumia <toolsntuts.space@gmail.com>
2025-05-17 12:25:12 +0100

    Update +layout.svelte

--- src/routes/(documents)/+layout.svelte
+++ src/routes/(documents)/+layout.svelte
@@ -2,7 +2,6 @@
 	import type { Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
 	import DocumentLayout from '$lib/components/ui/document-layout/document-layout.svelte';
-	import "../../app.css"
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 </script>
