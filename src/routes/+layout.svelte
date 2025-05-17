2860858e0eefc8fee7b546d8b3b850d22d644a4b
Jumia <toolsntuts.space@gmail.com>
2025-05-17 12:24:57 +0100

    push

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	import type { LayoutData } from './$types';
-	import "../app.css";
+	import '../app.css';
+	import Footer from '$lib/components/ui/footer/footer.svelte';
+	import Logo from './components/Logo.svelte';
+	import { ClerkProvider } from 'svelte-clerk';
+	import Header from '$lib/components/ui/header/header.svelte';
+	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+	import Wrap from '$lib/components/ui/wrap/wrap.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 
-	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+	let { children } = $props();
 </script>
 
-{@render children()}
+<ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+	<Header logo={Logo} />
+	<Wrap>
+		<div class="flex flex-col gap-4">
+			{@render children()}
+		</div>
+	</Wrap>
+	<Footer
+		slogan="Raising a total kingdom child"
+		logo={Logo}
+		organizationName="RCN Lagos Children Department"
+	/>
+	<ModeWatcher />
+</ClerkProvider>
